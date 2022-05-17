60e98c70cfe24332dcd3fd2f3d25dbff1bd921f4
jackbuoy <jacklotito@outlook.com>
2022-05-17 00:56:50 -0400

    Added correct color randomness
    -colors are now chosen from the list of  six options given in const colors
       -all other functions work smoothly
        with this change, including the
        squares changing to the correct
        color after a correct guess

--- main.js
+++ main.js
@@ -7,13 +7,25 @@ const colors = [
     "rgb(255, 255, 0)",
     "rgb(0, 255, 255)"
 ];
+const changeColors = (color) => {
+    squares.forEach((square) => {
+        square.style.backgroundColor = color;
+    })
+};
+
+const pickColor = () => {
+    //Get a random number between 0 and 5 inclusive
+    const random = Math.floor(Math.random() * colors.length);
+    //Return colors[ran]
+    return colors[random];
+}
 
 //Select elements
 const squares = document.querySelectorAll(".square");
 const colorDisplay = document.getElementById("colorDisplay");
 const message = document.getElementById("message");
 //Choose winning color
-let pickedColor = colors[0];
+let pickedColor = pickColor();
 
 //Update colorDisplay
 colorDisplay.textContent = pickedColor;
@@ -28,9 +40,11 @@ for(let i = 0; i < squares.length; i++) {
         //Compare that color to pickedColor
         if(clickedColor === pickedColor) {
             message.textContent = "Correct :)";
+            changeColors(pickedColor);
         } else {
             this.style.backgroundColor = "darkslategray";
             message.textContent = "Try again :O"
         }
     })
-}
+};
+
