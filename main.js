0cadc825cc6e8b2053b727e9214d5722077bb8ea
jackbuoy <jacklotito@outlook.com>
2022-05-17 01:21:32 -0400

    Added true random colors and a reset button
    -got rid of the static array of colors and made a function that returns an array of random colors for our game
    -the title background now also changes to the correct guessed color when it is chosen
    -added a reset button that creates a new array of colors and a new correct color to pick
    -some small name and format changes as well

--- main.js
+++ main.js
@@ -1,12 +1,4 @@
-//Constant declarations
-const colors = [
-    "rgb(255, 0, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 255)"
-];
+//Functions
 const changeColors = (color) => {
     squares.forEach((square) => {
         square.style.backgroundColor = color;
@@ -20,15 +12,45 @@ const pickColor = () => {
     return colors[random];
 }
 
+const generateRandomColor = () => {
+    //pick r, g, b values between 0 & 255
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+const generateRandomColors = (num) => {
+    let output = [];
+    for(let i = 0; i < num; i++) {
+        output.push(generateRandomColor());
+    }
+    return output;
+}
+
+colors = generateRandomColors(6);
+
 //Select elements
 const squares = document.querySelectorAll(".square");
 const colorDisplay = document.getElementById("colorDisplay");
 const message = document.getElementById("message");
+const title = document.querySelector("h1");
+const resetButton = document.getElementById("resetButton");
 //Choose winning color
 let pickedColor = pickColor();
 
 //Update colorDisplay
 colorDisplay.textContent = pickedColor;
+
+resetButton.addEventListener("click", function() {
+    colors = generateRandomColors(6);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    title.style.backgroundColor = "darkslategray";
+    for(let i = 0; i < colors.length; i++) {
+        squares[i].style.backgroundColor = colors[i];
+    };
+})
 
 //Set up squares
 for(let i = 0; i < squares.length; i++) {
@@ -41,6 +63,7 @@ for(let i = 0; i < squares.length; i++) {
         if(clickedColor === pickedColor) {
             message.textContent = "Correct :)";
             changeColors(pickedColor);
+            title.style.backgroundColor = pickedColor;
         } else {
             this.style.backgroundColor = "darkslategray";
             message.textContent = "Try again :O"
