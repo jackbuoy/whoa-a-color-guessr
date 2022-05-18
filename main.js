4d9e9dbd1a9ae27e1c8258c84c73e8f84be418a4
jackbuoy <jacklotito@outlook.com>
2022-05-17 22:45:49 -0400

    Difficulty buttons and modes added
    -added an easy and hardmode to the stripe that highlight when selected
    -the easy and hard buttons modify the game accordingly when clicked, easy mode has 3 squares to choose from while hard mode has 6 squares
    -reformatted comments for readability

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-//Functions
+//=======================
+// Functions
+//=======================
+
 const changeColors = (color) => {
     squares.forEach((square) => {
         square.style.backgroundColor = color;
@@ -6,14 +9,14 @@ const changeColors = (color) => {
 };
 
 const pickColor = () => {
-    //Get a random number between 0 and 5 inclusive
+    // Get a random number between 0 and 5 inclusive
     const random = Math.floor(Math.random() * colors.length);
-    //Return colors[ran]
+    // Return colors[ran]
     return colors[random];
 }
 
 const generateRandomColor = () => {
-    //pick r, g, b values between 0 & 255
+    // pick r, g, b values between 0 & 255
     const r = Math.floor(Math.random() * 256);
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
@@ -28,22 +31,35 @@ const generateRandomColors = (num) => {
     return output;
 }
 
-colors = generateRandomColors(6);
+//=======================
+// Initialize variables
+//=======================
 
-//Select elements
+// State
+let numSquares = 6;
+
+// Select elements
 const squares = document.querySelectorAll(".square");
 const colorDisplay = document.getElementById("colorDisplay");
 const message = document.getElementById("message");
 const title = document.querySelector("h1");
 const resetButton = document.getElementById("resetButton");
-//Choose winning color
+const easyButton = document.getElementById("easyButton");
+const hardButton = document.getElementById("hardButton");
+
+let colors = generateRandomColors(numSquares);
 let pickedColor = pickColor();
 
-//Update colorDisplay
+//=======================
+// Main
+//=======================
+
+// Update colorDisplay
 colorDisplay.textContent = pickedColor;
 
+// Reset button
 resetButton.addEventListener("click", function() {
-    colors = generateRandomColors(6);
+    colors = generateRandomColors(numSquares);
     pickedColor = pickColor();
     colorDisplay.textContent = pickedColor;
     title.style.backgroundColor = "darkslategray";
@@ -52,14 +68,45 @@ resetButton.addEventListener("click", function() {
     };
 })
 
-//Set up squares
+// Easy button
+easyButton.addEventListener("click", function() {
+    this.classList.add("selected");
+    hardButton.classList.remove("selected");
+    numSquares = 3;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for(let i = 0; i < squares.length; i++) {
+        if(colors[i]) {
+            squares[i].style.backgroundColor = colors[i];
+        }
+        else {
+            squares[i].style.backgroundColor = "darkslategray";
+        }
+    }
+})
+
+// Hard button
+hardButton.addEventListener("click", function() {
+    this.classList.add("selected");
+    easyButton.classList.remove("selected");
+    numSquares = 6;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for(let i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = colors[i];
+    }
+})
+
+// Set up squares
 for(let i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = colors[i];
-    //Add click listeners
+    // Add click listeners
     squares[i].addEventListener("click", function() {
-        //Get color of the clicked square
+        // Get color of the clicked square
         const clickedColor = this.style.backgroundColor;
-        //Compare that color to pickedColor
+        // Compare that color to pickedColor
         if(clickedColor === pickedColor) {
             message.textContent = "Correct :)";
             changeColors(pickedColor);
