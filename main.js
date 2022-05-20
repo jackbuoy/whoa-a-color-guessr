743c2ddbe45cf2052ba4ff33b0756810fceacf87
jackbuoy <jacklotito@outlook.com>
2022-05-19 23:02:58 -0400

    Finalizing interaction display changes and cleanup
    -lots of code refactoring and comment changes to keep things dry and readable
       -consolidated the difficulty button
         handlers and reset button function
       -created a main function which was
         not necessary but I wanted to
    -style changes to the fonts, color scheme, and stripe that incorporates the correct color choice better upon choosing it

--- main.js
+++ main.js
@@ -8,11 +8,11 @@ const changeColors = (color) => {
     })
 };
 
-const pickColor = () => {
-    // Get a random number between 0 and 5 inclusive
-    const random = Math.floor(Math.random() * colors.length);
-    // Return colors[ran]
-    return colors[random];
+const changeStripeColors = (color) => {
+    modeButtons.forEach((button) => {
+        button.style.color = color;
+    })
+    resetButton.style.color = color;
 }
 
 const generateRandomColor = () => {
@@ -31,12 +31,42 @@ const generateRandomColors = (num) => {
     return output;
 }
 
+const pickColor = () => {
+    // Get a random number from 0 to the number of squares
+    const random = Math.floor(Math.random() * colors.length);
+    // Return colors[ran]
+    return colors[random];
+}
+
+const reset = () => {
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    resetButton.textContent = "New Colors";
+    colorDisplay.textContent = pickedColor;
+    for(let i = 0; i < squares.length; i++) {
+        if(colors[i]) {
+            squares[i].style.backgroundColor = colors[i];
+        }
+        else {
+            squares[i].style.backgroundColor = "black";
+        }
+    }
+    title.style.backgroundColor = "slateblue";
+    resetButton.style.color = "slateblue";
+    modeButtons.forEach((button) => {
+        button.style.color = "slateblue";
+    })
+    message.textContent = "";
+}
+
 //=======================
 // Initialize variables
 //=======================
 
 // State
 let numSquares = 6;
+let colors = generateRandomColors(numSquares);
+let pickedColor = pickColor();
 
 // Select elements
 const squares = document.querySelectorAll(".square");
@@ -44,77 +74,55 @@ const colorDisplay = document.getElementById("colorDisplay");
 const message = document.getElementById("message");
 const title = document.querySelector("h1");
 const resetButton = document.getElementById("resetButton");
-const easyButton = document.getElementById("easyButton");
-const hardButton = document.getElementById("hardButton");
-
-let colors = generateRandomColors(numSquares);
-let pickedColor = pickColor();
+const modeButtons = document.querySelectorAll(".mode");
 
 //=======================
 // Main
 //=======================
 
-// Update colorDisplay
-colorDisplay.textContent = pickedColor;
-
-// Reset button
-resetButton.addEventListener("click", function() {
-    colors = generateRandomColors(numSquares);
-    pickedColor = pickColor();
+function main() {
+    // Update colorDisplay
     colorDisplay.textContent = pickedColor;
-    title.style.backgroundColor = "darkslategray";
-    for(let i = 0; i < colors.length; i++) {
+
+    // Reset button
+    resetButton.addEventListener("click", reset);
+
+    // Mode Buttons
+    modeButtons.forEach((button) => {
+        button.addEventListener("click", function() {
+            modeButtons[0].classList.remove("selected");
+            modeButtons[1].classList.remove("selected");
+            this.classList.add("selected");
+            if(this.textContent === "Easy") {
+                numSquares = 3;
+            }
+            else {
+                numSquares = 6;
+            }
+            reset();
+        });
+    });
+
+    // Set up squares
+    for(let i = 0; i < squares.length; i++) {
         squares[i].style.backgroundColor = colors[i];
+        // Add click listeners
+        squares[i].addEventListener("click", function() {
+            // Get color of the clicked square
+            const clickedColor = this.style.backgroundColor;
+            // Compare that color to pickedColor
+            if(clickedColor === pickedColor) {
+                message.textContent = "Correct";
+                changeColors(pickedColor);
+                title.style.backgroundColor = pickedColor;
+                changeStripeColors(pickedColor);
+                resetButton.textContent = "Play Again";
+            } else {
+                this.style.backgroundColor = "black";
+                message.textContent = "Try Again"
+            }
+        })
     };
-})
+}
 
-// Easy button
-easyButton.addEventListener("click", function() {
-    this.classList.add("selected");
-    hardButton.classList.remove("selected");
-    numSquares = 3;
-    colors = generateRandomColors(numSquares);
-    pickedColor = pickColor();
-    colorDisplay.textContent = pickedColor;
-    for(let i = 0; i < squares.length; i++) {
-        if(colors[i]) {
-            squares[i].style.backgroundColor = colors[i];
-        }
-        else {
-            squares[i].style.backgroundColor = "darkslategray";
-        }
-    }
-})
-
-// Hard button
-hardButton.addEventListener("click", function() {
-    this.classList.add("selected");
-    easyButton.classList.remove("selected");
-    numSquares = 6;
-    colors = generateRandomColors(numSquares);
-    pickedColor = pickColor();
-    colorDisplay.textContent = pickedColor;
-    for(let i = 0; i < squares.length; i++) {
-        squares[i].style.backgroundColor = colors[i];
-    }
-})
-
-// Set up squares
-for(let i = 0; i < squares.length; i++) {
-    squares[i].style.backgroundColor = colors[i];
-    // Add click listeners
-    squares[i].addEventListener("click", function() {
-        // Get color of the clicked square
-        const clickedColor = this.style.backgroundColor;
-        // Compare that color to pickedColor
-        if(clickedColor === pickedColor) {
-            message.textContent = "Correct :)";
-            changeColors(pickedColor);
-            title.style.backgroundColor = pickedColor;
-        } else {
-            this.style.backgroundColor = "darkslategray";
-            message.textContent = "Try again :O"
-        }
-    })
-};
-
+main();
